24909849c23ac5d99a8b5d72a65d04ca199cf31d
Milo CARTAL <105420069+milocartal@users.noreply.github.com>
2024-02-19 11:19:22 +0100

    Update PlayMenuManager.cs

--- Assets/Script/UI/PlayMenuManager.cs
+++ Assets/Script/UI/PlayMenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 
 public class PlayMenuManager : MonoBehaviour
@@ -11,10 +12,32 @@ public class PlayMenuManager : MonoBehaviour
     public GameObject ChoixCarMenuContainer;
     public GameObject MissionMenuContainer;
 
+    [SerializeField] private GameObject _grandPrix;
+    [SerializeField] private GameObject _courseSimple;
+    [SerializeField] private GameObject _chrono;
+    [SerializeField] private GameObject _challenge;
+    [SerializeField] private GameObject _tuto;
+    [SerializeField] private GameObject _retour;
+    [SerializeField] private GameObject _retourChoixCar;
+    [SerializeField] private GameObject _circuit1;
+    public SelectCarHandler _selectCarHandler;
+
+    void Update()
+    {
+        if (InputManager.Instance.MenuOpenCloseInput)
+        {
+            if (ChoixCarMenuContainer.activeSelf == true || CourseSimpleContainer.activeSelf == true)
+            {
+                Return_to_PlayMenu();
+            }
+        }
+    }
+
     public void GoToCarMenu()
     {
         PlayMenuMainContainer.SetActive(false);
         ChoixCarMenuContainer.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(_selectCarHandler.getBouttonVoiture());
     }
 
     public void GoToTrackMenu()
@@ -22,6 +45,7 @@ public class PlayMenuManager : MonoBehaviour
         CourseSimpleContainer.SetActive(true);
         //PlayMenuMainContainer.SetActive(false);
         ChoixCarMenuContainer.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(_circuit1);
     }
 
 
@@ -39,7 +63,8 @@ public class PlayMenuManager : MonoBehaviour
         {
             ChoixCarMenuContainer.SetActive(false);
             PlayMenuMainContainer.SetActive(true);
-            
+            EventSystem.current.SetSelectedGameObject(_grandPrix);
+
         }
         //Si on était dans le Menu Course et qu'on retourne en arrière
         if (CourseSimpleContainer.activeSelf == true)
@@ -47,12 +72,14 @@ public class PlayMenuManager : MonoBehaviour
             ChoixCarMenuContainer.SetActive(true);
             CourseSimpleContainer.SetActive(false);
             //PlayMenuMainContainer.SetActive(false);
+            EventSystem.current.SetSelectedGameObject(_selectCarHandler.getBouttonVoiture());
         }
 
         if (MissionMenuContainer.activeSelf == true)
         {
             PlayMenuMainContainer.SetActive(true);
             MissionMenuContainer.SetActive(false);
+            
         }
     }
 }
